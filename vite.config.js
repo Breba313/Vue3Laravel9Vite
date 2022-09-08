9e7edc352184b5b00b5e2f114c72d87821f254fe
Brenda Batista <Brebatista@gmail.com>
2022-09-08 09:11:01 -0500

    Remove secure server from vite config

--- vite.config.js
+++ vite.config.js
@@ -8,10 +8,7 @@ export default defineConfig({
         laravel({
             input: ['resources/css/app.css', 'resources/js/app.js'],
             refresh: true,
+            https: true,
         }),
     ],
-    server: { 
-        https: true, 
-        host: 'localhost', 
-    },
 });
