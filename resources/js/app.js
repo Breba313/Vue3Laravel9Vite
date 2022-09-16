b1b627ddad3e584d2f3df8f0b93e7716197778b7
Brenda Batista <Brebatista@gmail.com>
2022-09-16 10:35:13 -0500

    Update app.js

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,7 +1,7 @@
 import './bootstrap';
 
 import { createApp } from 'vue'
-import App from './layouts/App.vue'
+import App from './layouts/app.vue'
 import { createPinia } from 'pinia'
 import router from './router'
 
